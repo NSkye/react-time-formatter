d9573a91fca9ed9b128deaac2e782c34ee16124b
Nikolai <nskye@pm.me>
2025-05-04 02:22:30 +0300

    Add test for negative timestamp for DateTime

--- src/date-time/_test/DateTime.test.tsx
+++ src/date-time/_test/DateTime.test.tsx
@@ -3,10 +3,12 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 import { describe, expect, test } from 'vitest';
 
+import { YEAR } from '@shared/time-primitives';
+
 import { DateTime } from '../react/DateTime';
 
 describe('DateTime', () => {
-  test('Renders local time correctly', () => {
+  test('renders local time correctly', () => {
     const localDate = '2025-04-02T12:58:01';
     const expected = '12h 58m 1s';
 
@@ -21,7 +23,7 @@ describe('DateTime', () => {
     expect(screen.getByTestId('time').textContent).toBe(expected);
   });
 
-  test('Renders local date correctly, months are 1-indexed', () => {
+  test('renders local date correctly, months are 1-indexed', () => {
     const expected = 'year is 2025, month is 4 and date is 2';
 
     render(
@@ -35,7 +37,7 @@ describe('DateTime', () => {
     expect(screen.getByTestId('time').textContent).toBe(expected);
   });
 
-  test('Renders date with timezone offset correctly', () => {
+  test('renders date with timezone offset correctly', () => {
     const expected = '2025.04.02 12:00';
     const UTCPlus2 = -120; // UTC+2;
 
@@ -59,9 +61,17 @@ describe('DateTime', () => {
     expect(screen.getByTestId('time').textContent).toBe(expected);
   });
 
-  test('Renders date from timestamp', () => {
+  test('renders date from timestamp', () => {
     render(<DateTime date={0}>{({ year }) => <span data-testid="time">{year}</span>}</DateTime>);
 
     expect(screen.getByTestId('time').textContent).toBe('1970');
+  });
+
+  test('renders date from negative timestamp', () => {
+    render(
+      <DateTime date={-1 * YEAR}>{({ year }) => <span data-testid="time">{year}</span>}</DateTime>
+    );
+
+    expect(screen.getByTestId('time').textContent).toBe('1969');
   });
 });
